29c0f4aacef564d481a8dc9a8869a4ea6fc27097
Danielly Costa <daniellycmcosta@gmail.com>
2017-10-08 18:58:48 -0300

    leitura de documentos

--- main.cpp
+++ main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include<vector>
+#include <string>
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+#include <string>
+
 #include<screen.h>
 #include<reta.h>
 #include<retangulo.h>
@@ -8,7 +15,48 @@ using namespace std;
 
 int main(){
 
-    Screen s(20,20);
+    vector <FiguraGeometrica*> figuras;
+    vector <FiguraGeometrica*>::iterator it;
+
+    ifstream entrada;
+    string comando;
+    int x, y, larg, alt,x0,y0,raio;
+    char b;
+    bool fillmode;
+
+    entrada.open("C:/Users/do lar/Documents/GitHub/DCA1202_Trat_de_Classes_Abstratas/entrada.txt");
+    if(entrada.is_open()){
+        cout << "Arquivo aberto com sucesso\n";
+        getline(entrada, comando);
+        stringstream interpr(comando);
+        interpr >> comando;
+
+        if(comando == "dim"){
+            comando >> larg >> alt;
+            Screen s(larg,alt);
+        }
+        else if(comando == "brush"){
+            comando >> b;
+            s.setBrush(b);
+        }
+        else if(comando == "line"){
+            comando >> x0 >> y0 >> x >> y;
+            figuras.push_back(new Reta(x0,y0,x,y));
+        }
+        else if(comando == "rectangle"){
+            comando >> x >> y >> larg >> alt;
+            figuras.push_back(new Retangulo(x,y,larg,alt));
+        }
+        else if(comando == "circle"){
+            comando >> x0 >> y0 >> raio >> fillmode;
+            figuras.push_back(new Circulo(x0,y0,raio,fillmode));
+        }
+    }
+    else{
+        cout << "Falha na abertura do arquivo\n";
+    }
+
+
     s.setBrush('0');
     //Reta teste(0,0,0,9);
     Retangulo teste(1,1,10,5,1);
